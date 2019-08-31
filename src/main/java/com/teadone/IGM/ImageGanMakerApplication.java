01d2c2bc26ad2c1640521956d1f9ce9d8421c614
Won <teadone@naver.com>
2019-08-31 14:20:02 +0900

    init file commit

---
+++ src/main/java/com/teadone/IGM/ImageGanMakerApplication.java
@@ -0,0 +1,13 @@
+package com.teadone.IGM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageGanMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageGanMakerApplication.class, args);
+	}
+
+}
