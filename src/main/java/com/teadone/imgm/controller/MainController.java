c63155cf906733b844f9393f9e501e7ed6b8c028
WON <teadone@naver.com>
2019-09-23 18:24:30 +0900

    add regist

--- src/main/java/com/teadone/imgm/controller/MainController.java
+++ src/main/java/com/teadone/imgm/controller/MainController.java
@@ -1,18 +1,16 @@
 package com.teadone.imgm.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
-import org.python.jline.internal.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.teadone.imgm.member.MemberService;
 import com.teadone.imgm.member.MemberVO;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class MainController {
 	
@@ -27,17 +25,32 @@ public class MainController {
 	public String login() {
 		return "login";
 	}
+	@GetMapping(value = { "/regist" })
+	public String register() {
+		return "regist";
+	}
 	@RequestMapping(value="/loginPro" , method = RequestMethod.POST)
 	public String loginPro(MemberVO vo,HttpSession session) {
-		if(vo.getMemPw()==service.logIn(vo)) {
-			Log.debug(vo.getMemId());
-			session.setAttribute("user", vo.getMemId());
-			return "index";
+		if(vo.getMemPw().equals(service.logIn(vo))) {
+			session.setAttribute("user", vo.getMemId());	
+			return "redirect:/";
 		}
 		else {
 			return "login";
 		}
-		
-		
+	}
+	@RequestMapping(value="/registPro" , method=RequestMethod.POST)
+	public String regist(MemberVO vo) {
+		if(service.join(vo)==1) {
+			return "login";
+		}
+		else {
+			return "regist";
+		}
+	}
+	@GetMapping(value="logout")
+	public String logout(HttpSession session) {
+		session.invalidate();	
+		return "redirect:/";
 	}
 }
