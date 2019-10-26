aa9e599c762150064f73e07bc16403620cfbf23a
WON <teadone@naver.com>
2019-10-26 13:04:45 +0900

    add google login successful!.
    it will get your profile name from google

--- src/main/java/com/teadone/imgm/ApplicationSecurity.java
+++ src/main/java/com/teadone/imgm/ApplicationSecurity.java
@@ -1,26 +1,117 @@
 package com.teadone.imgm;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-  
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+
 @Configuration
-@EnableOAuth2Sso
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-     
-	
-	 @Override 
-	 protected void configure(HttpSecurity http) throws Exception { 
-		 http
-		 	.antMatcher("/**") 
-		 	.authorizeRequests() 
-		 	.antMatchers("/**") 
-		 	.permitAll() 
-		 	.anyRequest()
-		 	.authenticated();
-	 
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		//http.antMatcher("/**").authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+        .antMatchers("/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .oauth2Login()
+        .loginPage("/login")
+        .authorizationEndpoint()
+        .baseUri("/oauth2/authorize-client")
+        .authorizationRequestRepository(authorizationRequestRepository())
+        .and()
+        .tokenEndpoint()
+        .accessTokenResponseClient(accessTokenResponseClient())
+        .and()
+        .defaultSuccessUrl("/loginSuccess")
+        .failureUrl("/loginFailure");
+		/*
+		 * http.antMatcher("/**").authorizeRequests().antMatchers("/",
+		 * "/login**").permitAll().anyRequest()
+		 * .authenticated().and().exceptionHandling() .authenticationEntryPoint(new
+		 * LoginUrlAuthenticationEntryPoint("/")).and() .addFilterBefore(ssoFilter(),
+		 * BasicAuthenticationFilter.class); // logout http.logout()
+		 * .invalidateHttpSession(true) .clearAuthentication(true)
+		 * .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		 * .logoutSuccessUrl("/") .permitAll();
+		 */
+
 	}
-	 
+
+		@Bean
+	    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+	        return new HttpSessionOAuth2AuthorizationRequestRepository();
+	    }
+
+	    @Bean
+	    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+	        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
+	        return accessTokenResponseClient;
+	    }
+
+	    
+	    // additional configuration for non-Spring Boot projects
+	    private static List<String> clients = Arrays.asList("google", "facebook");
+
+	    //@Bean
+	    public ClientRegistrationRepository clientRegistrationRepository() {
+	        List<ClientRegistration> registrations = clients.stream()
+	            .map(c -> getRegistration(c))
+	            .filter(registration -> registration != null)
+	            .collect(Collectors.toList());
+
+	        return new InMemoryClientRegistrationRepository(registrations);
+	    }
+
+	    private static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
+
+	    @Autowired
+	    private Environment env;
+
+	    private ClientRegistration getRegistration(String client) {
+	        String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
+
+	        if (clientId == null) {
+	            return null;
+	        }
+
+	        String clientSecret = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-secret");
+	        if (client.equals("google")) {
+	            return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+	                .clientId(clientId)
+	                .clientSecret(clientSecret)
+	                .build();
+	        }
+	        if (client.equals("facebook")) {
+	            return CommonOAuth2Provider.FACEBOOK.getBuilder(client)
+	                .clientId(clientId)
+	                .clientSecret(clientSecret)
+	                .build();
+	        }
+	        return null;
+	    }
 }
--- src/main/java/com/teadone/imgm/controller/MainController.java
+++ src/main/java/com/teadone/imgm/controller/MainController.java
@@ -2,20 +2,36 @@ package com.teadone.imgm.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.teadone.imgm.ApplicationSecurity;
 import com.teadone.imgm.board.BoardService;
 import com.teadone.imgm.board.BoardVO;
 import com.teadone.imgm.image.ImageService;
@@ -24,168 +40,213 @@ import com.teadone.imgm.member.MemberService;
 import com.teadone.imgm.member.MemberVO;
 import com.teadone.imgm.util.GetFileList;
 
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private MemberService service;
 	@Autowired
 	private ImageService imgservice;
 	@Autowired
 	private BoardService boardservice;
+	@Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+    private static final String authorizationRequestBaseUri = "oauth2/authorize-client";
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+    
+		
+	
 	@GetMapping(value = { "/", "index" })
 	public String home(ModelMap modelmap) throws IOException {
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		log.debug(auth.toString());
-		//log.debug("principal:"+auth.getPrincipal().toString());
-		//log.debug("credential:"+auth.getCredentials());
-		//log.debug("details:"+auth.getDetails());
-		//log.debug("name:" +auth.getName());
-		//log.debug("Authorites:" +auth.getAuthorities());
-		List<String> f=GetFileList.getImgFileList("D:\\SpringWorks\\ImageGANMaker\\src\\main\\resources\\static\\Generate");
-		modelmap.put("gImg",f);
-		
+
+		//Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//log.debug(auth.toString());
+		// log.debug("principal:"+auth.getPrincipal().toString());
+		// log.debug("credential:"+auth.getCredentials());
+		// log.debug("details:"+auth.getDetails());
+		// log.debug("name:" +auth.getName());
+		// log.debug("Authorites:" +auth.getAuthorities());
+		List<String> f = GetFileList
+				.getImgFileList("D:\\SpringWorks\\ImageGANMaker\\src\\main\\resources\\static\\Generate");
+		modelmap.put("gImg", f);
+
 		return "index";
 	}
-	
-	@GetMapping(value = { "/login" }) 
-	public String login() {
-		 return "login"; 
+
+	@GetMapping(value = { "/login" })
+	public String login(Model model) {
+		Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+            .as(Iterable.class);
+        if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+
+        clientRegistrations.forEach(registration -> oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        log.debug("oauth2AuthenticationUrls:" + oauth2AuthenticationUrls);
+		
+		return "login";
 	}
-	 
-	
+	@GetMapping("/loginSuccess")
+    public String getLoginInfo(HttpSession session, OAuth2AuthenticationToken authentication) {
+
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+
+        String userInfoEndpointUri = client.getClientRegistration()
+            .getProviderDetails()
+            .getUserInfoEndpoint()
+            .getUri();
+
+        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+                .getTokenValue());
+
+            HttpEntity<String> entity = new HttpEntity<String>("", headers);
+
+            ResponseEntity<Map> response = restTemplate.exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+            Map userAttributes = response.getBody();
+            session.setAttribute("user", userAttributes.get("name"));
+            
+        }
+
+        return "redirect:/";
+    }
 	/*
 	 * @RequestMapping("login") public String login() { log.debug("auth실행");
 	 * Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 	 * log.debug(auth.getPrincipal().toString()); return "login"; }
 	 */
-	
-
 
 	@GetMapping(value = { "/regist" })
 	public String register() {
 		return "regist";
 	}
-	@RequestMapping(value="/loginPro" , method = RequestMethod.POST)
-	public String loginPro(MemberVO vo,HttpSession session) {
-		if(vo.getMemPw().equals(service.logIn(vo))) {
-			session.setAttribute("user", vo.getMemId());	
-			return "redirect:/";
-		}
-		else {
-			return "login";
-		}
-	}
-	@RequestMapping(value="/registPro" , method=RequestMethod.POST)
+
+	/*
+	 * @RequestMapping(value = "/loginPro", method = RequestMethod.POST) public
+	 * String loginPro(MemberVO vo, HttpSession session) { if
+	 * (vo.getMemPw().equals(service.logIn(vo))) { session.setAttribute("user",
+	 * vo.getMemId()); return "redirect:/"; } else { return "login"; } }
+	 */
+
+	@RequestMapping(value = "/registPro", method = RequestMethod.POST)
 	public String regist(MemberVO vo) {
-		if(service.join(vo)==1) {
+		if (service.join(vo) == 1) {
 			return "login";
-		}
-		else {
+		} else {
 			return "regist";
 		}
 	}
-	@GetMapping(value="logout")
+
+	@GetMapping(value = "logout")
 	public String logout(HttpSession session) {
-		session.invalidate();	
+		session.invalidate();
 		return "redirect:/";
 	}
-	@GetMapping(value="upload")
+
+	@GetMapping(value = "upload")
 	public String upload(HttpSession session) {
-		if(session.getAttribute("user") != null) {
+		if (session.getAttribute("user") != null) {
 			return "upload";
-		}
-		else
+		} else
 			return "login";
 	}
-	@RequestMapping(value="/UploadPro" , method= {RequestMethod.POST,RequestMethod.GET})
-	public String fileUpload(MultipartHttpServletRequest multipartHttpServletRequest,HttpSession session) {
-		if(session!=null) {
-			ImageVO vo=new ImageVO();
+
+	@RequestMapping(value = "/UploadPro", method = { RequestMethod.POST, RequestMethod.GET })
+	public String fileUpload(MultipartHttpServletRequest multipartHttpServletRequest, HttpSession session) {
+		if (session != null) {
+			ImageVO vo = new ImageVO();
 			vo.setMemId(session.getAttribute("user").toString());
-			log.debug(Integer.toString(imgservice.fileUpload(multipartHttpServletRequest , vo)));
-			
+			log.debug(Integer.toString(imgservice.fileUpload(multipartHttpServletRequest, vo)));
+
 			return "redirect:/";
-		}
-		else
+		} else
 			return "login";
 	}
-	@GetMapping(value="myImage")
-	public String myImage(ModelMap model,HttpSession session) {
-		if(session!=null) {
-			ImageVO vo=new ImageVO();
+
+	@GetMapping(value = "myImage")
+	public String myImage(ModelMap model, HttpSession session) {
+		if (session != null) {
+			ImageVO vo = new ImageVO();
 			vo.setMemId(session.getAttribute("user").toString());
 			model.put("gmImg", imgservice.getImageList(vo));
-		
+
 			return "myImage";
-		}
-		else {
+		} else {
 			return "login";
 		}
 	}
-	
-	@GetMapping(value="board")
+
+	@GetMapping(value = "board")
 	public String board(ModelMap model) {
-		
+
 		model.put("post", boardservice.getPosts());
 		return "board";
 	}
-	
-	@GetMapping(value="boardWrite")
+
+	@GetMapping(value = "boardWrite")
 	public String boardWrite(HttpSession session) {
-		if(session.getAttribute("user") != null) {
+		if (session.getAttribute("user") != null) {
 			return "boardWrite";
-		}
-		else
+		} else
 			return "login";
 	}
-	@RequestMapping(value="/writePost" , method= {RequestMethod.POST,RequestMethod.GET})
-	public String writePost(MultipartHttpServletRequest multipartHttpServletRequest,HttpSession session,BoardVO vo) {
-		if(session!=null) {	
+
+	@RequestMapping(value = "/writePost", method = { RequestMethod.POST, RequestMethod.GET })
+	public String writePost(MultipartHttpServletRequest multipartHttpServletRequest, HttpSession session, BoardVO vo) {
+		if (session != null) {
 			vo.setMemId(session.getAttribute("user").toString());
 			log.debug(Integer.toString(boardservice.writePost(vo)));
-			
+
 			return "redirect:/";
-		}
-		else
+		} else
 			return "login";
 	}
-	
-	@GetMapping(value="recentImage")
-	public String recentImage(ModelMap model,HttpSession session) {
-		
-		//model.put("gmImg", imgservice.getRecentImageList());
-		
+
+	@GetMapping(value = "recentImage")
+	public String recentImage(ModelMap model, HttpSession session) {
+
+		// model.put("gmImg", imgservice.getRecentImageList());
+
 		return "myImage";
 	}
-	
-	@GetMapping(value="myGenerateImage")
-	public String myGenerateImage(ModelMap model,HttpSession session) throws IOException {
-		if(session!=null) {
-			ImageVO vo=new ImageVO();
+
+	@GetMapping(value = "myGenerateImage")
+	public String myGenerateImage(ModelMap model, HttpSession session) throws IOException {
+		if (session != null) {
+			ImageVO vo = new ImageVO();
 			vo.setMemId(session.getAttribute("user").toString());
-			//model.put("gmImg", imgservice.getRecentImageList("D:\\SpringWorks\\ImageGANMaker\\src\\main\\resources\\static\\Generate",vo));
-			model.put("gmImg",GetFileList.getRecentFileList("D:\\SpringWorks\\ImageGANMaker\\src\\main\\resources\\static\\Generate",imgservice.getImageList(vo)));
+			// model.put("gmImg",
+			// imgservice.getRecentImageList("D:\\SpringWorks\\ImageGANMaker\\src\\main\\resources\\static\\Generate",vo));
+			model.put("gmImg",
+					GetFileList.getRecentFileList(
+							"D:\\SpringWorks\\ImageGANMaker\\src\\main\\resources\\static\\Generate",
+							imgservice.getImageList(vo)));
 			return "myGenerateImage";
-		}
-		else {
+		} else {
 			return "login";
 		}
 	}
+
 	@RequestMapping(value = "/board/{num}")
 	public String getContent(@PathVariable int num, ModelMap model) {
 		BoardVO vo = new BoardVO();
 		vo.setNum(num);
-		model.put("post",boardservice.getPost(vo));
+		model.put("post", boardservice.getPost(vo));
 
 		return "content";
 	}
-	@RequestMapping(value="personalInform")
+
+	@RequestMapping(value = "personalInform")
 	public String personalInform() {
 		return "personalInform";
 	}
